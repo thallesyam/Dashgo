e9d39d55d38730af01236aef92b2441ba95c419e
Thalles Ian <thallesyam@users.=.github.com>
2021-07-27 20:01:23 -0300

    feat: crate signin page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,27 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { Flex, Button, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
 
-export default function Home() {
+export default function SignIn() {
   return (
-    <div>
-      <h1>Dashgo</h1>
-    </div>
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p={8}
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input type="email" name="email" label="E-mail" />
+          <Input type="password" name="password" label="Senha" />
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   )
 }
