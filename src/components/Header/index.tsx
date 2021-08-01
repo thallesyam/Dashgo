84408642d24d3cc637d7b48f7fadd2cad1135504
Thalles Ian <thallesyam@users.=.github.com>
2021-08-01 13:34:12 -0300

    feat: header responsive

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, useBreakpointValue } from '@chakra-ui/react'
 
 import { Logo } from '../Header/Logo'
 import { SearchBox } from '../Header/SearchBox'
@@ -6,6 +6,11 @@ import { NotificationNav } from '../Header/NotificationNav'
 import { Profile } from '../Header/Profile'
 
 export function Header() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  })
+
   return (
     <Flex
       as="header"
@@ -19,12 +24,12 @@ export function Header() {
     >
       <Logo />
 
-      <SearchBox />
+      {isWideVersion && <SearchBox />}
 
       <Flex align="center" ml="auto">
         <NotificationNav />
 
-        <Profile />
+        <Profile showProfileData={isWideVersion} />
       </Flex>
     </Flex>
   )
