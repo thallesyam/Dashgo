7cb4dae844e8038e722a574d1703192eb8f50856
Thalles Ian <thallesyam@users.=.github.com>
2021-08-01 15:00:44 -0300

    feat: create responsive style and add links

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,9 +1,15 @@
-import { HStack, Button, Box } from '@chakra-ui/react'
+import { HStack, Stack, Box } from '@chakra-ui/react'
 import { PaginationItem } from './PaginationItem'
 
 export function Pagination() {
   return (
-    <HStack mt="8" justify="space-between" align="center" spacing="6">
+    <Stack
+      direction={['column', 'row']}
+      mt="8"
+      justify="space-between"
+      align="center"
+      spacing="6"
+    >
       <Box>
         <strong>0</strong> -<strong> 10</strong> de
         <strong> 100</strong>
@@ -16,6 +22,6 @@ export function Pagination() {
         <PaginationItem number={4} />
         <PaginationItem number={5} />
       </HStack>
-    </HStack>
+    </Stack>
   )
 }
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,14 +12,21 @@ import {
   Td,
   Tbody,
   Text,
+  useBreakpointValue,
 } from '@chakra-ui/react'
-import { RiAddLine, RiPencilLine } from 'react-icons/ri'
+import { RiAddLine } from 'react-icons/ri'
 
 import { Header } from '../../components/Header'
 import { Sidebar } from '../../components/Sidebar'
 import { Pagination } from '../../components/Pagination'
+import Link from 'next/link'
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  })
+
   return (
     <Box>
       <Header />
@@ -32,32 +39,34 @@ export default function UserList() {
             <Heading size="lg" fontWeight="normal">
               Usuários
             </Heading>
-            <Button
-              as="a"
-              size="sm"
-              fontSize="sm"
-              colorScheme="pink"
-              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-            >
-              Criar novo
-            </Button>
+
+            <Link href="/users/create" passHref prefetch>
+              <Button
+                as="a"
+                size="sm"
+                fontSize="sm"
+                colorScheme="pink"
+                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+              >
+                Criar novo
+              </Button>
+            </Link>
           </Flex>
 
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" width="8">
+                <Th px={['4', '4', '6']} color="gray.300" width="8">
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuários</Th>
-                <Th>Data de cadastro</Th>
-                <Th w={8}></Th>
+                {isWideVersion && <Th>Data de cadastro</Th>}
               </Tr>
             </Thead>
 
             <Tbody>
               <Tr>
-                <Td px="6">
+                <Td px={['4', '4', '6']}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -69,19 +78,7 @@ export default function UserList() {
                   </Box>
                 </Td>
 
-                <Td>04 de Abril de 2021</Td>
-
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
+                {isWideVersion && <Td>04 de Abril de 2021</Td>}
               </Tr>
             </Tbody>
           </Table>
