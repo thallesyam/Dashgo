fff3748d787a663b252fc7039080fd5d45015f91
Thalles Ian <thallesyam@users.=.github.com>
2021-08-01 13:59:34 -0300

    feat: create menu mobile and context Sidebar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,15 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react'
+import { Flex, useBreakpointValue, IconButton, Icon } from '@chakra-ui/react'
+
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext'
 
 import { Logo } from '../Header/Logo'
 import { SearchBox } from '../Header/SearchBox'
 import { NotificationNav } from '../Header/NotificationNav'
 import { Profile } from '../Header/Profile'
+import { RiMenuLine } from 'react-icons/ri'
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer()
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -22,6 +26,17 @@ export function Header() {
       px="6"
       align="center"
     >
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open Navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        ></IconButton>
+      )}
+
       <Logo />
 
       {isWideVersion && <SearchBox />}
