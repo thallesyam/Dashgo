579d4cdc96153b507abba0730aff6ca776885e43
Thalles Ian <thallesyam@users.=.github.com>
2021-08-08 18:51:02 -0300

    feat: create mirage pagination

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,16 +1,16 @@
-import { createServer, Factory, Model } from 'miragejs'
+import { createServer, Factory, Model, Response } from 'miragejs'
 import faker from 'faker'
 
 type User = {
-  name: string;
-  email: string;
+  name: string
+  email: string
   created_at: string
 }
 
-export function makeServer () {
+export function makeServer() {
   const server = createServer({
     models: {
-      user: Model.extend<Partial<User>>({})
+      user: Model.extend<Partial<User>>({}),
     },
 
     factories: {
@@ -23,8 +23,8 @@ export function makeServer () {
         },
         createdAt() {
           return faker.date.recent(10)
-        }
-      })
+        },
+      }),
     },
 
     seeds(server) {
@@ -35,12 +35,31 @@ export function makeServer () {
       this.namespace = 'api'
       this.timing = 750
 
-      this.get('/users')
+      this.get('/users', function (schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams
+
+        const total = schema.all('user').length
+
+        const pageStart = (Number(page) - 1) * Number(per_page)
+        const pageEnd = pageStart + Number(per_page)
+
+        const users = this.serialize(schema.all('user')).users.slice(
+          pageStart,
+          pageEnd
+        )
+
+        return new Response( 
+          200, 
+          { 'x-total-count': String(total) },
+          { users }  
+        )
+      })
+
       this.post('/users')
-    
+
       this.namespace = ''
       this.passthrough()
-    }
+    },
   })
 
   return server
